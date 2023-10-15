50f3cb6e2c7eecc9727425667c6c258a723642f5
Shaikh Khaled <khankhaled501@gmail.com>
2023-10-15 23:47:18 +0530

    Update timeLockedContract.sol

--- 14thDay./timeLockedContract.sol
+++ 14thDay./timeLockedContract.sol
@@ -1,1 +1,13 @@
-D
+function withdrawFunds(address recipient, uint256 amount) public {
+  // Checking  if the caller is authorized to withdraw funds.
+  require(hasRole(ROLE_WITHDRAWER, msg.sender), "Unauthorized withdrawal");
+
+  // Checking  if the withdrawal time has elapsed.
+  require(block.timestamp >= unlockTime, "Withdrawal time has not elapsed");
+
+  // Checking  if the contract has enough funds to withdraw.
+  require(address(this).balance >= amount, "Insufficient balance");
+
+  // Transfering the funds to the recipient.
+  recipient.transfer(amount);
+}
